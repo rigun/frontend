cf23f2a4f51b71e2be97d0eb00625095a0b650d6
vianhandika <32483035+vianhandika@users.noreply.github.com>
2018-01-07 23:23:09 +0700

    Revert "Popups Fix"
    This reverts commit ebc4c2456e7eecb1df4720d5aafc2be567bc4783.

--- Backup/HOMEPAGE/js/main.js
+++
@@ -1,9 +0,0 @@
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var myLatLng = {lat: -7.779355,lng: 110.415776};
-  var mapOptions = {
-    center: new google.maps.LatLng(-7.779355, 110.415776,17), zoom: 17
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-  var marker = new google.maps.Marker({position:myLatLng,map: map,});
-}
